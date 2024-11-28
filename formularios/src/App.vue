806a94000ac6a1a4961da4da747d5763c44f67e0
Noemy <n.rivera2221@gmail.com>
2024-11-27 22:49:17 -0300

    agregue img

---
+++ formularios/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import Formulario from './components/Formulario.vue';
+import Tarjeta from './components/Tarjeta.vue';
+export default {
+  data() {
+    return {
+      datos: {} // Inicializa un objeto vacío para almacenar los datos de la tarjeta
+    };
+  },
+  components:{
+    Formulario,
+    Tarjeta
+  },
+  methods: { //actualiza los datos
+    actualizarDatos(datos) {
+      // console.log('Datos recibidos en App.vue:', datos);
+      this.datos = { ...datos };  // Asigna los datos al objeto 'datos'
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="form">
+    <Formulario @datos-actualizados="actualizarDatos"/> <!-- Escucha el evento 'datos-actualizados' y llama al método actualizarDatos -->
+    <Tarjeta :datos="datos" /> <!-- Pasa los datos al componente Tarjeta -->
+  </div>
+</template>
+<style scoped>
+  .form{
+    display: flex;
+    gap: 15px;
+  }
+</style>
---
+++ formularios/src/components/Formulario.vue
@@ -0,0 +1,77 @@
+<script>
+    export default {
+        name: "Formulario",
+            // Función de datos que devuelve un objeto que representa el estado del componente
+            data(){
+                return{
+                    formulario: {
+                        titulo: '',
+                        chip: '',
+                        numero: '',
+                        fechaExpiracion: '',
+                        propietario: '',
+                        tipoSRC: ''
+                    }
+                }
+            },
+            methods: {
+                // Función de datos que devuelve un objeto que representa el estado del componente
+                emitData() {
+                    console.log(this.formulario);
+                    this.$emit('datos-actualizados', { ...this.formulario }); // Emitir todo el objeto 'formulario'
+                }
+            }
+};
+</script>
+<template>
+    <form @submit.prevent="submitForm">
+        <div>
+            <label>Título de la tarjeta: </label>
+            <input v-model="formulario.titulo"  @input="emitData" />
+        </div>
+        <div>
+            <label>Chip SRC: </label>
+            <input v-model="formulario.chip"  @input="emitData"/>
+        </div>
+        <div>
+            <label>Número: </label>
+            <input v-model="formulario.numero" @input="emitData"  />
+        </div>
+        <div>
+            <label>Fecha de expiración: </label>
+            <input v-model="formulario.fechaExpiracion" @input="emitData" />
+        </div>
+        <div>
+            <label>Propietario: </label>
+            <input v-model="formulario.propietario" @input="emitData" />
+        </div>
+        <div>
+            <label>Tipo de tarjeta SRC: </label>
+            <input v-model="formulario.tipoSRC" @input="emitData" />
+        </div>
+    </form>
+
+</template>
+<style scoped>
+    form {
+        padding: 18px;
+        border-radius: 20px;
+        border: ridge 1px;
+        box-shadow: 1px 1px 1px 1px gray;
+        font-weight: bold;
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        justify-content: center;
+    }
+    form div {
+        display: grid;
+        grid-template-columns: 100px 200px;
+        align-items: center;
+        gap: 5px;
+    }
+
+    form div label {
+        text-align: right;
+    }
+</style>
